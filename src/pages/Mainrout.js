4be0bc2069e0ac77e803a65ba02eadc770477488
sreevishnu01 <sreevishnu01@gmail.com>
2021-12-09 19:25:25 +0400

    homepage and hash

--- src/pages/Mainrout.js
+++ src/pages/Mainrout.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar1 from "../components/Layout/Nav/Navbar";
 import Home from "./Home";
 import Post from "./Post";
